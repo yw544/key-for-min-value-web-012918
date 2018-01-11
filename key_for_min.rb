34183c54705bbe93f70ccd0d36a1a9efcf4acfad
York Wang <yw544@nyu.edu>
2018-01-11 10:22:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+smallest_k = nil
+smallest_v = nil
+name_hash.each do |key, value|
+  if smallest_v == nil || value<smallest_v
+    smallest_v = value
+    smallest_k = key
+  end
+end
+smallest_k
 end
